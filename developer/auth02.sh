275807c552a42451c92769a6f5163d4585c64afd
alphanetEX <fixtexhax@gmail.com>
2019-03-28 17:28:49 -0300

    adding  docker and shifter shell sintaxis installation in auth02.sh

--- developer/auth02.sh
+++ developer/auth02.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-passwd=$(echo -e "odroid\n")
+passwd= $(echo -e "odroid\n") #verified code but this patron is equivalent to instalation 
 repeatwd=""
 usergithub=""
 passparse=""
@@ -36,10 +36,13 @@ $passw | sudo -S timedatectl set-timezone  $timezone
 $passw | sudo -S apt-get install ntpdate
 $passw | sudo -S ntpdate-debian
 
+$pass | sudo -S etckeeper init
+$pass | sudo -S etckeeper commit -m "Initial checkin"
+
 
 #asignacion de contrasenia 
 echo -e  "$passwd\n$repeatwd\n$repeatwd" | passwd
-
+passw= $(echo -e "$repeatwd\n") 
 #configuracion de la wlan 
 $passw | sudo -S apt install wireless-tools -y
 $passw | sudo -S apt install wpasupplicant -y
@@ -55,6 +58,8 @@ $passw | sudo -S cp -r /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 $passw | sudo -S cp -r packages/jail.local /etc/fail2ban/
 $passw | sudo -S /etc/init.d/ssh restart
 
+$pass | sudo -S etckeeper commit "Basic Security Completed"
+
 #Configuracion de entorno de desarrollo 
 git config --global user.mail $email
 git config --global user.name $usergithub
@@ -64,6 +69,8 @@ echo "\n" | sh-keygen -t rsa -b 4096 -C $email -P $passwd_sshkey
 #-C asignacion de correo electronico 
 #-P asignacion de contrasenia 
 
+$passw | sudo -S etckeeper commit "Git Github/Gitlab basic enviroment completed"
+
 #configuracion de nvm y npm final de comanderia 
 $passw | sudo -S apt-get install -y curl build-essential libssl-dev git
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
@@ -71,6 +78,46 @@ $passw | su - odroid
 nvm ls-remote 
 nvm install v10.15.3
 npm -v && node -v 
+
+$passw | sudo -S etckeeper commit "nvm with node and npm dependencias installed"
+
+#configurating docker instalation 
+$passw | sudo -S apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=armhf] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+$passw | sudo -S apt-get install docker-ce docker-ce-cli containerd.io
+$passw | sudo -S groupadd docker
+$passw | usermod -aG docker $USER
+$passw | sudo -S etckeeper commit "Docker installation was succesfull"
+
+#odroid shifter shell instalation 
+$passw | sudo -S apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+git clone https://github.com/hardkernel/wiringPi
+cd wiringPi/
+./build 
+
+$passw | sudo -S etckeeper commit "WiringPI and Shifter Shield configuration was success"
+#modificacion de ohmyzsh 
+$passw | sudo -S apt-get install ccze -y 
+#dmesg | ccze -A  example to using  log colors with  dmesg 
+
+
 
 echo "script terminado!"
 echo "passwd user :$repeatwd  passwd_sshkey: $passparse wlanpasswd:$wlanpasswd"  
